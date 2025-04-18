4b106b0b8c4bae1ac30be7c926db53e97efca09c
olgaivanovagaula <olgaivanova2512@yandex.ru>
2025-04-18 20:14:41 +0300

    4

--- src/components/web.ts
+++ src/components/web.ts
@@ -1,53 +1,48 @@
 import { Api as BaseApi } from './base/api';
-
 import {
-	OrderPayload as OrderPayload,
-	OrderResponse as OrderResponse,
-	ProductData as ProductData,
-	ApiResponseList as ProductListResponse,
+ ApiResponseList as ProductListResponse,
+ OrderPayload as OrderPayload,
+ ProductData as ProductData,
+ OrderResponse as OrderResponse,
 } from '../types';
 
 export interface AppApiInterface {
-	fetchProductList: () => Promise<ProductData[]>;
-	processOrderSubmission: (payload: OrderPayload) => Promise<OrderResponse>;
+ processOrderSubmission: (payload: OrderPayload) => Promise<OrderResponse>;
+
+ fetchProductList: () => Promise<ProductData[]>;
 }
 
 export class AppApi extends BaseApi implements AppApiInterface {
-	readonly contentDeliveryUrl: string;
+ readonly contentDeliveryUrl: string;
 
-	constructor(
-		apiBaseUrl: string,
+ constructor(
+  contentDeliveryUrl: string,
+  apiBaseUrl: string,
+  config?: RequestInit
+ ) {
+  super(apiBaseUrl, config);
+  this.contentDeliveryUrl = contentDeliveryUrl;
+ }
 
-		contentDeliveryUrl: string,
-
-		config?: RequestInit
-	) {
-		super(apiBaseUrl, config);
-		this.contentDeliveryUrl = contentDeliveryUrl;
-	}
-
-	fetchProductList(): Promise<ProductData[]> {
-		return this.get('/product')
-
-			.then((data: ProductListResponse<ProductData>) =>
-				data.items.map((item) => ({
-					...item,
-					image: `${this.contentDeliveryUrl}${item.image}`,
-				}))
-			)
-
-			.catch((err) => {
-				console.error('aElisst:', err);
-				throw new Error('Fai');
-			});
-	}
-
-	processOrderSubmission(payload: OrderPayload): Promise<OrderResponse> {
-		return this.post('/order', payload)
-			.then((response: OrderResponse) => response)
-			.catch((err) => {
-				console.error('Err order:', err);
-				throw new Error('on failed.');
-			});
-	}
+ processOrderSubmission(payload: OrderPayload): Promise<OrderResponse> {
+  return this.post('/order', payload)
+   .then((response: OrderResponse) => response)
+   .catch((err) => {
+    console.error('Error submitting order:', err);
+    throw new Error('Order submission failed.');
+   });
+ }
+ fetchProductList(): Promise<ProductData[]> {
+  return this.get('/product')
+   .then((data: ProductListResponse<ProductData>) =>
+    data.items.map((item) => ({
+     ...item,
+     image: `${this.contentDeliveryUrl}${item.image}`,
+    }))
+   )
+   .catch((err) => {
+    console.error('Error fetching product list:', err);
+    throw new Error('Failed to retrieve product list.');
+   });
+ }
 }
